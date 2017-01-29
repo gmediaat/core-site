ba72796342b29d77fa627d0a1378cd22d133b749
Felix Gradinaru <felix@g-media.at>
2017-01-29 14:33:09 +0100

    Initial Commit

---
+++ Classes/Gmedia/CoreSite/ViewHelpers/SiteHeaderViewHelper.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Gmedia\CoreSite\ViewHelpers;
+
+class SiteHeaderViewHelper extends \TYPO3\Fluid\Core\ViewHelper\AbstractViewHelper {
+        /**
+         * Render the title and apply some magic
+         *
+         * @param string $containerClass container class
+         * @param string $title title of the site header
+         * @param string $lead lead text of the site header
+         * @param string $leadClass class of the lead text
+         * @param string $buttons html of the content section
+         * @return string rendered html
+         * @throws \TYPO3\Fluid\Core\ViewHelper\Exception
+         */
+        public function render($containerClass = "site-header-inner", $orientation = 0, $title = NULL, $lead = NULL, $leadClass = "lead", $buttons = NULL) {
+
+                // Create Object ID
+                
+                
+                $output = '<div class="'.$containerClass.'">';
+				
+				if($title != "" && $title != NULL) {
+					$output .= '<h1>'.$title.'</h1>';
+				}
+				
+				if($lead != "" && $lead != NULL) {
+					$output .= '<p class="'.$leadClass.'">'.$lead.'</p>';
+					
+				}
+				
+				if($buttons != "" && $buttons != NULL) {
+					$output .= $buttons;
+				}
+					
+				$output .= '</div>';
+				
+				return $output;
+        }
+}	
+	
+?>
